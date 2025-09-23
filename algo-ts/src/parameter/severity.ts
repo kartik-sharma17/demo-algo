30661ef2b5aa2f9bbe5b73df3a0ab0e29c58e56d
Kartik Sharma <kartiksharma88103@gmail.com>
2025-09-23 12:14:40 +0530

    define the parameter and weightage

---
+++ algo-ts/src/parameter/severity.ts
@@ -0,0 +1,36 @@
+
+
+export const severity = [
+    {
+        key: "veryLow",
+        weight: "1.5"
+    },
+    {
+        key: "low",
+        weight: "2.5"
+    },
+    {
+        key: "belowAverage",
+        weight: "3.7"
+    },
+    {
+        key: "moderate",
+        weight: "5"
+    },
+    {
+        key: "aboveModerate",
+        weight: "6.3"
+    },
+    {
+        key: "high",
+        weight: "7.5"
+    },
+    {
+        key: "veryHigh",
+        weight: "8.7"
+    },
+    {
+        key: "elite",
+        weight: "10"
+    }
+]
